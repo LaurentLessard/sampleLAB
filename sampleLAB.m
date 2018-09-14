5ce9dd63fcb2022391cb7a1a18d2883996a33dad
Raginii <Raginii@users.noreply.github.com>
2018-09-14 17:05:09 -0500

    Added filtering using eps

--- sampleLAB.m
+++ sampleLAB.m
@@ -1,9 +1,10 @@
-function lab = sampleLAB(delta)
+function [lab, rgb, xyY] = sampleLAB(delta)
     % sampleLAB(delta): Generates a grid of points uniformly spaced in LAB space 
     % which can be mapped to RGB space.
     % Input : delta -- TODO
     % Output : Nx3 matrix of coordinates in xyY space.
-    close all; clc;
+    clf;close all; clc;
+    
     % Generate delta spaced points for L (0-100),a(-100 to 100) and b (-100 to 100)
     L = 0:delta:100 ;% length = N
     a = [0-delta:-delta:-100 0:delta:100];
@@ -13,27 +14,36 @@ function lab = sampleLAB(delta)
     figure;  subplot(1,2,1);set(gcf, 'Position', get(0, 'Screensize'));
     scatter3(lab(:,2),lab(:,3),lab(:,1),10,'k');
     hold on;
+    
     % Every row of the matrix 'lab' is a point in the color space
     % Validate coordinates to exist in RGB space
     rgb = lab2rgb(lab);
-    [row,~] = find(rgb > 1 | rgb < 0);
+    [row,~] = find(rgb > 1+eps | rgb < 0-eps);
     indices = unique(row);
     % Get rid of coordinates which do not exist in RGB space
     rgb(indices,:) = [] ;
     lab(indices,:) = [];
+    
+    % Plot the selected points
     scatter3(lab(:,2),lab(:,3),lab(:,1),80,rgb,'filled');
     daspect([1 1 1]); axis([-100 100 -100 100 0 100]); grid on; 
     title(['LAB space with uniformly distributed points for \delta = ',num2str(delta)]);
     legend(["Uniformly distributed points","Selected points existing in RBG space"],'Location','southeast');
     xlabel('a');ylabel('b'); zlabel('L');
     hold off;
+    
+    % Convert Lab to xyY
     out = colorconvert(lab,'Lab','D65');
     xyY = [out.x out.y out.Y];
-    lab = xyY;
+    
+    % Plot colors in xyY space
     subplot(1,2,2);set(gcf, 'Position', get(0, 'Screensize'));
-    scatter3(xyY(:,1),xyY(:,2),xyY(:,3),70,rgb,'filled');
+    scatter3(xyY(:,1),xyY(:,2),xyY(:,3),70,rgb,'filled','MarkerEdgeColor',[0 0 0]);
     axis([0 1 0 1 0 100]); daspect([0.1 0.1 10]);
     title(['Selected colors in xyY space for \delta = ',num2str(delta)]);
     xlabel('x');ylabel('y'); zlabel('Y');
-    savefig(strcat('fig_delta_',num2str(delta),'.fig'));
+    txt = ['# Colors: ',num2str(length(rgb(:,1)))]
+    text(1,0,100,txt)
+    % Save figure
+    savefig(strcat('output/fig_delta_',num2str(delta),'.fig'));
 end
